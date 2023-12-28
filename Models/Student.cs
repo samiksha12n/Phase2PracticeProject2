1a24eae6dbeb35b1222d838daa647f79ca89cc3d
samiksha12n <samikshaunesarkar@gmail.com>
2023-12-28 11:11:17 +0530

    Practice Practice Project MVC

---
+++ Models/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace StudentDataMvc.Models
+{
+    [Table("Student")]
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public int Class { get; set; }
+        [Required]
+        public string Subject { get; set; }
+        
+    }
+}
